74ae753690ef1cf7b3f2121882ac3986ea59dbff
Muh. Naufal Bimasbuqin <bmsbqn@gmail.com>
2024-12-04 21:59:00 +0800

    Create penjumlahan.py

---
+++ penjumlahan.py
@@ -0,0 +1,4 @@
+bilangan1 = int(input("Masukkan bilangan pertama: "))
+bilangan2 = int(input("Masukkan bilangan kedua: "))
+hasil = bilangan1 + bilangan2
+print("Hasil penjumlahan dari", bilangan1, "dan", bilangan2, "adalah", hasil)
